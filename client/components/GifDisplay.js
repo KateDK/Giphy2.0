729481cc4518f03735287234a69da3f5eefa4ca8
Kate Dubitski <kate.dubitski@gmail.com>
2018-12-04 19:17:48 -0500

    gif display is now a class component

--- client/components/GifDisplay.js
+++ client/components/GifDisplay.js
@@ -1,16 +1,18 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
 /**
  * COMPONENT
  */
-export const GifDisplay = props => {
-  return (
-    <div>
-      <h3>Gif List will be rendered here! YAY</h3>
-    </div>
-  )
+class GifDisplay extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Gif List will be rendered here! YAY</h3>
+      </div>
+    )
+  }
 }
 
 /**
