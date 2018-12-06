778b17c59d192e3e8bb9480e7ed77864c34ae124
Kate Dubitski <kate.dubitski@gmail.com>
2018-12-06 13:36:09 -0500

    created a component to render the quote if no search results available

---
+++ client/components/NoSearchRes.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+/**
+ * COMPONENT
+ */
+export const NoSearchRes = () => {
+  return (
+    <div>
+      <h3>
+        "Most discoveries even today are a combination of serendipity and of
+        searching."
+      </h3>
+      <a
+        href="https://en.wikipedia.org/wiki/Siddhartha_Mukherjee"
+        target="_blank"
+      >
+        <p>-Siddhartha Mukherjee</p>
+      </a>
+    </div>
+  )
+}
+
+export default NoSearchRes
