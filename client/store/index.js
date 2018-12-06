19652ac5f02418cb04139e7b83de7044de4e8702
Kate Dubitski <kate.dubitski@gmail.com>
2018-12-05 21:28:44 -0500

    deleted redundant code from store

--- client/store/index.js
+++ client/store/index.js
@@ -2,7 +2,6 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import user from './user'
 import gifs from './gifs'
 
 const reducer = combineReducers({gifs})
